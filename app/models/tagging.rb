2343bb3a3a708a49dc66de3469e899f9868ac824
Raghu Betina <raghu@firstdraft.com>
2016-11-22 05:34:49 +0000

    Validate uniqueness of taggings.tag_id

--- app/models/tagging.rb
+++ app/models/tagging.rb
@@ -5,6 +5,8 @@ class Tagging < ApplicationRecord
 
   # Validations
 
+  validates :tag_id, :uniqueness => { :scope => [:restaurant_id], :message => "is already tagged with this" }
+
   validates :tag_id, :presence => true
 
 end
