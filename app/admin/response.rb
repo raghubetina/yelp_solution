16f249674c11e2a2d406923b63200b5e0ceefd6c
Raghu Betina <raghu@firstdraft.com>
2016-11-22 05:35:28 +0000

    Whitelist columns in Response for ActiveAdmin

--- app/admin/response.rb
+++ app/admin/response.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Response do
 
+ permit_params :review_id, :ownership_id, :body
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
